821c4b26f89e923c771392e72a849bda68ef171d
Matteo Tocci <matteo.tocci2004@gmail.com>
2025-12-02 17:45:39 +0100

    primo commit

---
+++ backend_healthconnect/src/main/java/com/backend_healthconnect/BackendHealthconnectApplication.java
@@ -0,0 +1,13 @@
+package com.backend_healthconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHealthconnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendHealthconnectApplication.class, args);
+    }
+
+}
---
+++ backend_healthconnect/src/test/java/com/backend_healthconnect/BackendHealthconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_healthconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendHealthconnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
