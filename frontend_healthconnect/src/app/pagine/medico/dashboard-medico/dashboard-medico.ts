9b6d65665d70f9585a131822e2247a88b78d9b8f
crxstqn <lprcst04h30c588y@studenti.unical.it>
2025-12-08 21:18:46 +0100

    modifiche css

--- frontend_healthconnect/src/app/pagine/medico/dashboard-medico/dashboard-medico.ts
+++ frontend_healthconnect/src/app/pagine/medico/dashboard-medico/dashboard-medico.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {StatCard} from '../components/stat-card/stat-card';
 
 @Component({
   selector: 'app-dashboard-medico',
-  imports: [],
+  imports: [
+    StatCard
+  ],
   templateUrl: './dashboard-medico.html',
   styleUrl: './dashboard-medico.css',
 })
