55a96b05872c30fc9026407e25497b5d694b3be0
crxstqn <lprcst04h30c588y@studenti.unical.it>
2025-12-11 18:52:38 +0100

    messaggio di ringraziamento per averci scelto medico

--- frontend_healthconnect/src/app/pagine/medico/components/messaggi/messaggi.ts
+++ frontend_healthconnect/src/app/pagine/medico/components/messaggi/messaggi.ts
@@ -1,6 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {MessaggioDTO} from '../../../../model/messaggioDTO';
-import {DatePipe, NgForOf, NgIf, SlicePipe} from '@angular/common';
+import {NgForOf, NgIf, SlicePipe} from '@angular/common';
 import {RouterLink} from '@angular/router';
 import {TimeAgoPipe} from '../../../../pipes/time-ago-pipe';
 
@@ -10,7 +10,6 @@ import {TimeAgoPipe} from '../../../../pipes/time-ago-pipe';
     NgIf,
     RouterLink,
     NgForOf,
-    DatePipe,
     SlicePipe,
     TimeAgoPipe
   ],
