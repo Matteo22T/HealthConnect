99e4eeadcb89c550a72b1af75a90fa37490f3261
Matteo Tocci <matteo.tocci2004@gmail.com>
2025-12-12 16:32:46 +0100

    inizio assistente ai

---
+++ frontend_healthconnect/src/app/pagine/paziente/assistente-ai/assistente-ai.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assistente-ai',
+  imports: [],
+  templateUrl: './assistente-ai.html',
+  styleUrl: './assistente-ai.css',
+})
+export class AssistenteAi {
+
+}
