1bb49533b573b1058de0157e297b937f4b40344f
crxstqn <lprcst04h30c588y@studenti.unical.it>
2026-01-13 13:20:48 +0100

    fixato scroll on top ora funzionante

--- frontend_healthconnect/src/app/app.config.ts
+++ frontend_healthconnect/src/app/app.config.ts
@@ -8,7 +8,6 @@ import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
-// --- 1. AGGIUNGI QUESTE IMPORTAZIONI FONDAMENTALI ---
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import {AuthService} from './service/auth-service';
@@ -19,13 +18,11 @@ export const appConfig: ApplicationConfig = {
     provideRouter(
       routes,
       withInMemoryScrolling({
-        scrollPositionRestoration: 'top',
+        scrollPositionRestoration: 'enabled',
         anchorScrolling: 'enabled',
       })
     ),
     provideHttpClient(withInterceptorsFromDi()),
-
-    // --- 2. IL TUO BLOCCO CALENDARIO (Ora funzionerà perché hai gli import sopra) ---
     importProvidersFrom(
       CalendarModule.forRoot({
         provide: DateAdapter,
